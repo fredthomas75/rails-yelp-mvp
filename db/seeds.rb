daa2028baf78ea6238c17893a3071c93728bd2e3
Frederic Thomas <fred@mt2lab.com>
2019-08-14 13:55:57 -0400

    Yelp final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+10.times { Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address, category: category.sample) }
